2fa9109a7a41015e344324f2f67223d7379b5fa8
mauritshdb <mauritshudaibi@gmail.com>
2023-12-09 21:45:45 +0700

    add index or main file

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const colors = require('colors')
+const {rooter} = require('./router/route')
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/v1/", rooter)
+
+
+app.listen(3000, () => {
+    console.log('[INFO] Server nyala dengan port 3000'.green);
+})
