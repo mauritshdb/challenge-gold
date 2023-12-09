171a64818b8fa548d4b7fdd764a0d2026d862ed3
mauritshdb <mauritshudaibi@gmail.com>
2023-12-09 21:45:34 +0700

    add router

---
+++ router/route.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const rooter = express.Router()
+
+const { Controller } = require("../controller.js")
+
+rooter.route("/products").get(Controller.getAllProducts).post(Controller.addProduct)
+rooter.route("/products/:id").get(Controller.getProductById).put(Controller.updateProductById).delete(Controller.deleteProduct)
+
+rooter.route("/addproduct").post(Controller.addProduct)
+rooter.route("/adduser").post(Controller.registerUser)
+
+rooter.route("/users").get(Controller.getAllUsers)
+rooter.route("/users/:id").get(Controller.getUserById).put(Controller.updateUserById).delete(Controller.deleteUser)
+
+module.exports = { rooter }
